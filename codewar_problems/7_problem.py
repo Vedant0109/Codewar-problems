337d848c1943959725dbcf44c7f23dc7b531b843
Vedant <tarunincarnate@gmail.com>
2024-11-16 06:07:58 +0000

    Added new problem

---
+++ codewar_problems/7_problem.py
@@ -0,0 +1,34 @@
+def solution():
+  '''A square of squares
+You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+
+Task
+Given an integral number, determine if it's a square number:
+
+In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+Examples
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false'''
+  number = 64
+  for i in range(number):
+    if i**2==number:
+      square_number= True
+      break
+    else:
+      square_number= False
+  if square_number:
+    print("It is square number")
+  else:
+    print("It is not a square number")
+
+
+solution()
